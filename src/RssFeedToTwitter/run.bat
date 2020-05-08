a3f53a205746bad859e194d16ffe3da196097c9c
Sanchit Kumar <sanchitkum@gmail.com>
2020-05-08 20:02:31 +0530

    Add run.bat script to deploy the console application as a WebJob on Azure App Service

---
+++ src/RssFeedToTwitter/run.bat
@@ -0,0 +1,4 @@
+REM The batch script is only intended to run the application
+REM Azure WebJobs supports running batch scripts.
+REM Include this batch script with the console application package/zip to deploy to Azure as a WebJob.
+dotnet RssFeedToTwitter.dll
