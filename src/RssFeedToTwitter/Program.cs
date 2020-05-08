8bfc3fea57f34b78f375ee24d4cc39299406d84e
Sanchit Kumar <sanchitkum@gmail.com>
2020-05-08 12:58:45 +0530

    Initial Commit: Initialize a console project using 'dotnet new console' and include .gitignore.

---
+++ src/RssFeedToTwitter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RssFeedToTwitter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
